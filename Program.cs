c8f67136a0b4f1f7056ca6cb372c887ef302fb14
MaFar5B <mafar5b@hotmail.com>
2023-07-18 18:26:40 +0800

    11
    1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Gaokao
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new ApplicationForm());
+            Student student = new Student();
+        }
+    }
+
+    public class Student
+    {
+        public String subject1;
+        public String subject2;
+        public String subject3;
+        public String wish;
+        public int score;
+        public int rank;
+    }
+}
