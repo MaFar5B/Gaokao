a9cf98b7e576c4916e27e4baf09c40bf0e7c13e9
MaFar5B <mafar5b@hotmail.com>
2023-07-18 15:55:59 +0800

    1
    1

--- gaokao.py
+++ gaokao.py
@@ -1,7 +1,12 @@
-class Application():
+from process import getRank
+
+class Student():
     def __init__(self):
         self.subject1
         self.subject2
         self.subject3
+        self.score
 
 if __name__ == "__main__":
+    print("started")
+    print(getRank(666))
---
+++ process.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+
+warnings.filterwarnings('ignore')
+plt.rcParams["font.sans-serif"]=["SimHei"]
+plt.rcParams["axes.unicode_minus"]=False
+
+data1 = pd.read_csv("G:\Documents\GitHub\Gaokao\Data\grade_line.csv")
+
+def getRank(score):
+    index = data1[data1["分数段"] == score].index[0]
+    return data1["累计人数"][index]
