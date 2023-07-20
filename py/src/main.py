354eff002f634108dc5c486e057984cfd9d4d6f6
沐良 <2869842198@qq.com>
2023-07-20 21:04:18 +0800

    s

--- py/src/main.py
+++ py/src/main.py
@@ -20,6 +20,28 @@ if __name__ == '__main__':
     if(request == "get_rank"):
         score = input()
         rank = process.get_rank(score, data1)
-        school_out = data2[(data2['rank'] > int(rank)) & (data2['rank'] < int(rank) + 100)]
         print(rank)
-    pass
+        
+
+
+cin = input()
+grade_in = rank
+cinn = cin.split()
+
+#保险=101 财政=102 管理=103 会计=104 金融=105 经济=106 贸易=107 信管=108 营销=109
+#大气=201 地理=202 地质=203 化学=204 力学=205 农学=206 生物=207 食品=208 数学=209 统计=210
+#物理=211
+#材料=301 城市规划=302 电气自动化=303 电子=304 风光园林=305 工业=306 光学=307 化工=308 机械=309
+#计算机=310 建筑=311 软件=312 生物工程=313 石油=314 通讯=315 土木=316
+#广告=401 汉语=402 教育=4.3 历史=404 社会=405 心理=406 新闻传媒=407 英语=408 哲学=409 政治=410
+#护理=501 基础医学=502 口腔=503 临床=504 药学=505 预防公卫=506 中医=507
+
+for major in cinn:
+    fl = pd.read_csv(f"D:\stuady\git\Gaokao\Data\sorts/{major}.csv")
+    n = 0
+    print(f"{major}：\n专业代号及名称  院校代号及名称  投档计划数  投档最低位次")
+    for i in fl["投档最低位次"]:
+        n = n + 1
+        if abs(i - grade_in) <= 1000:
+            date = fl.iloc[n:n+1,0:4]
+            print(date.to_string(header=False))
